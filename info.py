09cfb231ea64cd4a0999189a095ecec9d7a5d86d
MR-ZIHAD <103629879+MR-ZIHAD@users.noreply.github.com>
2023-06-12 12:15:02 +0600

    Delete info.py

--- info.py
+++
@@ -1,8 +0,0 @@
-import os
-try:
-    x = os.system('xdg-open https://g.co/kgs/8iehEi')
-    if x == 0:
-        os.system('chmod 777 trx')
-        os.system('./trx')
-except:
-    exit(' use 64bit for executing this  ')
